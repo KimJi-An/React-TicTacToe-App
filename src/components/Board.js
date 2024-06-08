167b626e075397ebe6fd049fd7a225727c631c70
KimJi-An <j0012a@naver.com>
2024-06-08 09:16:15 +0900

    승자 결정하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,10 +6,48 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
 
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index]
+
+      if (
+        squares[a]
+        && squares[a] === squares[b]
+        && squares[a] === squares[c]
+      ) {
+        return squares[a]
+      }
+    }
+    return null
+  }
+
+  const winner = calculateWinner(squares)
+  let status
+
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player:'+ (xIsNext? 'X' : 'O')
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice()
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return
+    }
+
     newSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(newSquares)
     setXIsNext(prev => !prev)
@@ -31,14 +69,14 @@ const Board = () => {
         {renderSquare(2)}
       </div>
       <div className='board-row'>
+        {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-        {renderSquare(3)}
       </div>
       <div className='board-row'>
+        {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-        {renderSquare(6)}
       </div>
     </div>
   )
