e5513bbc54abc06e7ad1aae41b0d7fa6b32e9762
KimJi-An <j0012a@naver.com>
2024-06-08 21:53:39 +0900

    JumpTo 함수 작성하기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Board from './components/Board'
 function App() {
   const [history, setHistory] = useState([ {squares: Array(9).fill(null)}])
   const [xIsNext, setXIsNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0)
 
   const calculateWinner = (squares) => {
     const lines = [
@@ -31,7 +32,7 @@ function App() {
     return null
   }
 
-  const current = history[history.length - 1]
+  const current = history[stepNumber]
   const winner = calculateWinner(current.squares)
 
   let status
@@ -43,15 +44,19 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice()
+    const newHistory = history.slice(0, stepNumber + 1)
+    const newCurrent = newHistory[newHistory.length - 1]
+    const newSquares = newCurrent.squares.slice()
 
     if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
 
     newSquares[i] = xIsNext? 'X' : 'O'
-    setHistory([...history, { squares: newSquares }])
+    setHistory([...newHistory, { squares: newSquares }])
     setXIsNext(prev => !prev)
+
+    setStepNumber(newHistory.length)
   }
 
   const moves = history.map((step, move) => {
@@ -59,11 +64,16 @@ function App() {
       'Go to move #' + move :
       'Go to game start'
       return (
-        <li>
-          <button>{desc}</button>
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{desc}</button>
         </li>
       )
   })
+
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext((step % 2) === 0)
+  }
 
   return (
     <div className="game">
